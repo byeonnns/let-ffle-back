98f0ff2a1827941cbd407af0f75e86626b550833
ryan <ljwryan@gmail.com>
2024-06-13 21:07:02 +0900

    Refactor: RaffleDetail 컬럼 수정

--- src/main/java/com/mycompany/let_ffle/dto/RaffleDetail.java
+++ src/main/java/com/mycompany/let_ffle/dto/RaffleDetail.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class RaffleDetail {
 	private int rno;
 	private String mid;
-	private Timestamp rdtcreatedat; // 래플 참여 시간
+	private Timestamp rdtenteredat; // 래플 참여 시간
 	private boolean rdtmissioncleared; // 미션 참여 여부
 	private int rdtberryspend; // 사용 베리 수
 }
