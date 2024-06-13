7fad750d890af8394369ff59a899ba428c0627c6
byeonnns <tjdwns3823@naver.com>
2024-06-13 14:15:51 +0900

    Feat: Board, Notice 서비스 추가

--- src/main/java/com/mycompany/let_ffle/LetffleBackendApplication.java
+++ src/main/java/com/mycompany/let_ffle/LetffleBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LetFfleBackendApplication {
+public class LetffleBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LetFfleBackendApplication.class, args);
+		SpringApplication.run(LetffleBackendApplication.class, args);
 	}
 
 }
