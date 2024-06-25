cef370726f55ad61b519cd1e9d9b8b924a4dd423
GIM-YOUNG-JOO <explicit13@naver.com>
2024-06-25 09:22:34 +0900

    refactor : 머지 할게요

--- src/main/java/com/mycompany/let_ffle/dao/RaffleDao.java
+++ src/main/java/com/mycompany/let_ffle/dao/RaffleDao.java
@@ -1,5 +1,8 @@
 package com.mycompany.let_ffle.dao;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.mycompany.let_ffle.dto.Raffle;
@@ -10,4 +13,6 @@ public interface RaffleDao {
 	public int insertRaffle(Raffle raffle);
 
 	public Raffle selectByRno(int rno);
+
+	public Map<String, BigDecimal> selectBySeqRno();
 }
--- src/main/java/com/mycompany/let_ffle/service/RaffleService.java
+++ src/main/java/com/mycompany/let_ffle/service/RaffleService.java
@@ -1,5 +1,9 @@
 package com.mycompany.let_ffle.service;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +13,10 @@ import com.mycompany.let_ffle.dao.RaffleImageDao;
 import com.mycompany.let_ffle.dao.TimeMissionDao;
 import com.mycompany.let_ffle.dto.request.RaffleRequest;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class RaffleService {
 	@Autowired
 	RaffleDao raffleDao;
@@ -21,16 +28,22 @@ public class RaffleService {
 	QuizMissionDao quizMissionDao;
 	
 	
-	public void insertRaffle(RaffleRequest raffleRequest){
-		
+	public RaffleRequest insertRaffle(RaffleRequest raffleRequest){
 		raffleDao.insertRaffle(raffleRequest.getRaffle());
+		Map<String, BigDecimal> resultMap = new HashMap<>();
+		resultMap = raffleDao.selectBySeqRno();
+		int rno = resultMap.get("LAST_NUMBER").intValue()-1;
+		log.info("현재 시퀀스 : " + resultMap.get("LAST_NUMBER"));
+		raffleRequest.getRaffleImage().setRno(rno);
+		raffleRequest.getTimeMission().setRno(rno);
 		raffleImageDao.insertRaffleImage(raffleRequest.getRaffleImage());
-		
 		if(raffleRequest.getTimeMission() != null) {
 			timeMissionDao.insertTimeMisson(raffleRequest.getTimeMission());
 		} else {
 			quizMissionDao.insertQuizMisson(raffleRequest.getQuizMission());
 		}
+		
+		return raffleRequest;
 	}
 
 	public RaffleRequest getRaffle(int rno) {
